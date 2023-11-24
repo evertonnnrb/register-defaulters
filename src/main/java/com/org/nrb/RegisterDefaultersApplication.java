36c86d2637b2ea4b24748ffac99c963b1be5c4e1
Everton Ribeiro <everton.nnrb@gmail.com>
2023-11-23 20:10:02 -0400

    setup

---
+++ src/main/java/com/org/nrb/RegisterDefaultersApplication.java
@@ -0,0 +1,13 @@
+package com.org.nrb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterDefaultersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterDefaultersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/nrb/RegisterDefaultersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.nrb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterDefaultersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
